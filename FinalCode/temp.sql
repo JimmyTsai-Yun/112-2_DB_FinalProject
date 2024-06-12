a4d49d2d8ff507a81dc188026e8593a85717d613
JimmyTsai-Yun_Mac <r12521709@ntu.edu.tw>
2024-06-12 10:39:56 +0800

    re-organize files

--- FinalCode/temp.sql
+++
@@ -1,5 +0,0 @@
--- SELECT * FROM calculate_mixed_with_timerange('NVDA', ARRAY['APPL', 'ABT'], TRUE, '2012-01-01', '2012-01-10', ARRAY[1,2,3]);
-SELECT * FROM calculate_dtw_with_timerange('NVDA', ARRAY['APPL', 'ABT', 'BAC'], TRUE, '2012-01-01', '2012-01-10');
--- SELECT * FROM calculate_pure_euclidean_results('NVDA', ARRAY['NVDA'], TRUE);
--- SELECT * FROM calculate_vshift_results('NVDA', ARRAY['NVDA'], TRUE);
--- SELECT count(*) FROM company_stock_prices where company = 'APPL';
